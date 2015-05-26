56145581a589327935e097cd7b2e14bd51b235de
karthikvaluebound <karthik@valuebound.com>
2015-05-26 18:44:43 +0530

    modified theme files

---
+++ sites/all/themes/semi/template.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+* theme_menu_link()
+*/
+//function semi_menu_link(array $variables) {
+//   $variables['element']['#attributes']['class'][] = 'menu-' . $variables['element']['#original_link']['mlid'];
+//   $variables['element']['#localized_options']['attributes']['class'][] = 'menu-' . $variables['element']['#original_link']['mlid'];
+//   return theme_menu_link($variables);
+//}
+
+?>
